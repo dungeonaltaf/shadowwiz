4576ae0e7b6d0b0d5f0ca3e1f9d095105ac56945
Altaf Hussain <altaf.hussain@royalbrothers.com>
2021-01-18 21:09:40 +0530

    auto color detection

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
+
+import analyze from 'rgbaster';
+
 function shadowwiz(options){
-    let images = document.querySelectorAll('shadowwiz');
+  let images = document.querySelectorAll('img.shadowwiz');
 
-    if (options.shadow_type=='hard'){
-      options.shadow_type = '0px';
-    }
-    else{
-      options.shadow_type = '20px'
-    }
-
-    images.forEach(image =>{
-      image.style.boxShadow = `20 px 20px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
-      if (options.padding){
-        image.style.padding = '1em';
-      }
-  
-    });
-
-   
+  if (options.shadow_type=='hard'){
+    options.shadow_type = '0px';
+  }
+  else{
+    options.shadow_type = '20px'
   }
 
+  images.forEach(image =>{
+    console.log(image);
+    console.log(image.src);
+    const result = await analyze(image.src); 
+    const color =result[0].color;
+    image.style.boxShadow = `20px 20px ${options.shadow_type} 1px ${color}`;
+    if (options.padding){
+      image.style.padding = '1em';
+    }
 
-  module.exports.shadowwiz = shadowwiz;
+  });
+}
+
+
+module.exports.shadowwiz = shadowwiz;
