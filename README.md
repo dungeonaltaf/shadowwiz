20b8a1689f7be2c09a5ec1161e6752564eecb3d4
Altaf Hussain <altaf.hussain@royalbrothers.com>
2021-01-18 19:40:17 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# shadowwiz
+# What is this?
+
+
+One of the world class shadow boxes for designers 
+
+
+# Installation
+
+`npm - shadowwiz --save`
+
+Then...
+
+```
+import {shadowwiz} from 'shadowwiz';
+shadowwiz({
+  shadow_type: 'soft'
+  padding: 'false'
+});
+```
+
+## Options
+
+
+Shadowwiz supports 2 optionsm both of which are optional:
+
+
+* *shadow_type* - _hard / soft_ (Defaults to soft)
+* *padding*- _boolean_ (Defaults to false)
---
+++ index.js
@@ -0,0 +1,22 @@
+function shadowwiz(options){
+    let images = document.querySelectorAll('shadowwiz');
+
+    if (options.shadow_type=='hard'){
+      options.shadow_type = '0px';
+    }
+    else{
+      options.shadow_type = '20px'
+    }
+
+    images.forEach(image =>{
+      image.style.boxShadow = `20 px 20px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
+    });
+
+    if (options.padding){
+      image.style.padding = '1em';
+    }
+
+  }
+
+
+  module.exports.shadowwiz = shadowwiz;
